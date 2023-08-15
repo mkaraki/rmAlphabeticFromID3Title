5b056b58249c7b65f9f1fac77d1b1c08b910bd5f
mkaraki <26180919+mkaraki@users.noreply.github.com>
2023-08-16 03:32:56 +0900

    Initial

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using rmAlphabeticFromID3Title;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+foreach (string fileName in Directory.GetFiles(".", "*.flac", SearchOption.AllDirectories))
+{
+	using (var f = TagLib.File.Create(fileName))
+	{
+		string title = f.Tag.Title;
+
+		int startIdx = 0;
+		int endIdx = title.Length - 1;
+
+		for (int i = 0; i < title.Length; i++)
+		{
+			if (Util.IsCharNotAlphabetic(title[i]))
+			{
+				startIdx = i;
+				if (Util.IsNotAlphabeticBeforeBracket(title, i))
+					endIdx--;
+				break;
+			}
+		}
+
+		for (int i = title.Length - 1; i >= 0; i--)
+		{
+			if (Util.IsCharNotAlphabetic(title[i]))
+			{
+				endIdx = i;
+				if (Util.IsNotAlphabeticAfterBracket(title, i))
+					endIdx++;
+				break;
+			}
+		}
+
+		int endLen = endIdx - startIdx + 1;
+
+		string newTitle = title.Substring(startIdx, endLen);
+
+		Console.WriteLine("\"{0}\"\n\t\"{1}\" -> \"{2}\"", fileName, title, newTitle);
+
+		f.Tag.Title = newTitle;
+		f.Save();
+	}
+}
---
+++ Util.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace rmAlphabeticFromID3Title
+{
+	internal static class Util
+	{
+		internal static bool IsCharNotAlphabetic(char c)
+			=> !Regex.IsMatch(c.ToString(), @"^([a-z]|\s|,|\.|'|\(|\)|-)$", RegexOptions.IgnoreCase);
+
+		internal static bool IsNotAlphabeticBeforeBracket(string s, int pos)
+		{
+			if (pos == 0) return false;
+			if (s[pos - 1] != ')' && s[pos - 1] != '(') return false;
+			// if [pos - 1] char (before bracket) is NOT alphabetic, that bracket shouldn't deleted:
+			//     return true
+			if (IsCharNotAlphabetic(s[pos])) return true;
+			else return false;
+		}
+
+		internal static bool IsNotAlphabeticAfterBracket(string s, int pos)
+		{
+			if (pos == s.Length - 1) return false;
+			if (s[pos + 1] != ')' && s[pos + 1] != '(') return false;
+			if (IsCharNotAlphabetic(s[pos])) return true;
+			else return false;
+		}
+	}
+}
